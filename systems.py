bc908b4cd8835e0156d71a7e44f8d060f13a895a
Jueri <jueri.keller@smail.th-koeln.de>
2021-03-16 15:41:31 +0100

    add ranker for livivo

--- systems.py
+++ systems.py
@@ -1,47 +1,23 @@
 import os
 import json
 import jsonlines
-import pysolr
 import subprocess
-import requests
-
-
-def load_json(directory, id_field):
-    for path, subdir, file in os.walk(directory):
-        extensions = tuple([".jsonl"])
-        files = [f for f in file if f.endswith(extensions)]
-        for f in files:
-            with jsonlines.open(os.path.join(path, f), 'r') as reader:
-                for obj in reader:
-                    yield {
-                        '_op_type': 'index',
-                        '_id': obj[id_field],
-                        '_source': obj}
-
-
-def load_settings(settings_path):
-    with open(settings_path) as json_file:
-        return json.load(json_file)
-
+import urllib
 
 class Ranker(object):
 
     def __init__(self):
-        self.core = 'idx'
-        self.base_url = 'http://localhost:8983'
+        self.core = 'livivo'
+        self.base_url = 'http://0.0.0.0:8983'
         self.core_url = '/solr/{}/'.format(self.core)
-        self.index_path = '/opt/solr-8.8.1/index/'
-        self.config = 'server/solr/configsets/livivo'
-
-        # self.index_settings_path = os.path.join('index_settings', 'livivo_settings.json')
         self.documents_path = '/data/livivo/documents/'
 
     def test(self):
         return 200
 
     def index(self):
-
-        subprocess.call("/opt/solr-8.8.1/bin/solr create_core -c livivo -d server/solr/configsets/livivo".split())
+        command = "/opt/solr-8.8.1/bin/solr create_core -c {} -d server/solr/configsets/livivo".format(self.core)
+        subprocess.call(command.split())
 
         files = [os.path.join(self.documents_path, f) for f in os.listdir(self.documents_path) if os.path.isfile(os.path.join(self.documents_path, f))]
 
@@ -49,17 +25,7 @@ class Ranker(object):
             command = "/opt/solr-8.8.1/bin/post -c livivo "+file
             subprocess.call(command.split())
 
-
-# bin/solr create_core -c test -d server/solr/configsets/livivo
-        #
-        # self.es.indices.create(index=self.INDEX, body=load_settings(self.index_settings_path))
-        #
-        # for success, info in helpers.parallel_bulk(self.es, load_json(self.documents_path, 'DBRECORDID'),
-        #                                            index=self.INDEX):
-        #     if not success:
-        #         return 'A document failed: ' + info, 400
-
-        return 'Index built with ' + ' docs', 200
+        return 'Index built', 200
 
     def rank_publications(self, query, page, rpp):
 
@@ -67,17 +33,23 @@ class Ranker(object):
         start = page * rpp
 
         if query is not None:
-            results = self.solr.search(query, **{'indent': 'on',
-                                                 'wt': 'json',
-                                                 'fl': 'score,cord_uid',
-                                                 'rows': start})
+            params = ['indent=on',
+                      'wt=json',
+                      'fl=DBRECORDID,score',
+                      'rows=' + str(rpp),
+                      'start=' + str(start)]
 
-            # from_ = start,
-            # size = rpp,
+            solrParams = '&'.join(params)
 
-            for result in results:
+            solr_query = 'TITLE:' + '(' + query + ')'
+
+            solrURL = self.base_url + self.core_url + "select?" + solrParams + "&q=" + str(urllib.parse.quote(solr_query))
+            response = urllib.request.urlopen(solrURL)
+            results = json.loads(response.read().decode('utf-8'))
+
+            for result in results['response']['docs']:
                 try:
-                    itemlist.append(result['DBRECORDID'])
+                    itemlist.append(result)
                 except:
                     pass
 
